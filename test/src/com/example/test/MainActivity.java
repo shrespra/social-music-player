abb4ce40714cd5ffb2664711324970d1a294b43f
Danish Shrestha <danish.shrestha@digi.com>
2013-12-25 15:16:51 -0600

    Added a simple file printer

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -6,27 +6,49 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-private List<File> getSongs() {
-	List<File> files = new ArrayList<File> ();
-	
-	
-	return files;
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		Log.i("Songs", "File names:");
+		List<File> files = getSongs();
+		for(File file : files) {
+			Log.i("Songs", file.getAbsolutePath());
+		}
+		
+		setContentView(R.layout.activity_main);
+	}
+
+	private List<File> getSongs() {
+		List<File> files = new ArrayList<File>();
+		
+		//root
+		File file = new File("/");
+		File[] children = file.listFiles();
+		
+		//print a list of all files in root folder
+		for(File child : children) {
+			Log.i("Songs", "Root child " +  child.getAbsolutePath());
+		}
+		//use recursion and add file to files list on
+		/*
+		if(child.getName().endsWith("mp3")) {
+			files.add(child);
+		}
+		*/
+		return files;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
